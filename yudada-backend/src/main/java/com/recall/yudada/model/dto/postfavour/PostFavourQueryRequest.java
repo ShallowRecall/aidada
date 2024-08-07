e94bc53ed85163f963ad8b5bf1a08cbda6c092cb
Recall <1989424334@qq.com>
2024-08-07 17:41:35 +0800

    精简初始化模版

---
+++ yudada-backend/src/main/java/com/recall/yudada/model/dto/postfavour/PostFavourQueryRequest.java
@@ -0,0 +1,30 @@
+package com.recall.yudada.model.dto.postfavour;
+
+import com.recall.yudada.common.PageRequest;
+import com.recall.yudada.model.dto.post.PostQueryRequest;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 帖子收藏查询请求
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class PostFavourQueryRequest extends PageRequest implements Serializable {
+
+    /**
+     * 帖子查询请求
+     */
+    private PostQueryRequest postQueryRequest;
+
+    /**
+     * 用户 id
+     */
+    private Long userId;
+
+    private static final long serialVersionUID = 1L;
+}
