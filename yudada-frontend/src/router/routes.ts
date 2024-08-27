f8ca59ee61b89619339adc841bf6f0d9682cb237
Recall <1989424334@qq.com>
2024-08-27 20:17:38 +0800

    完成用户管理

--- yudada-frontend/src/router/routes.ts
+++ yudada-frontend/src/router/routes.ts
@@ -3,6 +3,9 @@ import HomeView from "@/views/HomeView.vue";
 import UserLayout from "@/layouts/UserLayout.vue";
 import ACCESS_ENUM from "@/access/accessEnum";
 import NoAuthPage from "@/views/NoAuthPage.vue";
+import UserLoginPage from "@/views/user/UserLoginPage.vue";
+import UserRegisterPage from "@/views/user/UserRegisterPage.vue";
+import AdminUserPage from "@/views/admin/AdminUserPage.vue";
 
 export const routes: Array<RouteRecordRaw> = [
   {
@@ -11,19 +14,19 @@ export const routes: Array<RouteRecordRaw> = [
     component: HomeView,
   },
   {
+    path: "/admin/user",
+    name: "用户管理",
+    component: AdminUserPage,
+    meta: {
+      access: ACCESS_ENUM.USER,
+    },
+  },
+  {
     path: "/noAuth",
     name: "无权限",
     component: NoAuthPage,
     meta: {
       hideInMenu: true,
-    },
-  },
-  {
-    path: "/admin",
-    name: "管理页面",
-    component: HomeView,
-    meta: {
-      access: ACCESS_ENUM.USER,
     },
   },
   {
@@ -38,16 +41,19 @@ export const routes: Array<RouteRecordRaw> = [
     path: "/user",
     name: "用户",
     component: UserLayout,
+    meta: {
+      hideInMenu: true,
+    },
     children: [
       {
         path: "/user/login",
         name: "用户登录",
-        component: HomeView,
+        component: UserLoginPage,
       },
       {
         path: "/user/register",
         name: "用户注册",
-        component: HomeView,
+        component: UserRegisterPage,
       },
     ],
   },
