ed8573d9ecc5cca55914d9bca1ae286ddf50dd9c
Recall <1989424334@qq.com>
2024-08-27 16:00:38 +0800

    完成前端模版搭建，引入axios,自动生成请求代码

---
+++ yudada-frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <template v-if="route.path.startsWith('/user')">
+      <router-view />
+    </template>
+    <template v-else>
+      <BasicLayout />
+    </template>
+  </div>
+</template>
+
+<script setup lang="ts">
+import BasicLayout from "@/layouts/BasicLayout.vue";
+import { useRoute } from "vue-router";
+import { useLoginUserStore } from "@/store/userStore";
+
+const route = useRoute();
+
+const loginUserStore = useLoginUserStore();
+loginUserStore.fetchLoginUser();
+</script>
+
+<style scoped></style>
